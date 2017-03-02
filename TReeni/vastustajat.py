0efde7cf91aef2a97fd2c80f58472984d0123210
niko <niko@Niko-PC.lan>
2017-03-02 18:47:29 +0200

    Initial Commit

---
+++ TReeni/vastustajat.py
@@ -0,0 +1,28 @@
+'''Vastustajat:
+FatisagNiko
+JesseJ
+
+Tehty 02.03.2017
+'''
+
+class Vastustaja:
+    def __init__(self, nimi, elamapisteet, vahinko):
+        self.nimi = nimi
+        self.elamapisteet = elamapisteet
+        self.vahinko = vahinko
+        
+def onko_elossa(self):
+    return self.elamapisteet > 0
+
+class FatisagNiko(Vastustaja):
+    def __init__(self):
+        super().__init__(nimi="Fätisäg Niko", elamapisteet=15, vahinko=2)
+        
+class JesseJ(Vastustaja):
+    def __init__(self):
+        super().__init__(nimi="Jessie J", elamapisteet=30, vahinko=15)
+        
+class antti_Kaskimaki(Vastustaja):
+    def __init__(self):
+        super().__init__(nimi="Antti Kaskimäki", elamapisteet=50, vahinko 20)
+
